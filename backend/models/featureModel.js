741d3226e7f1a4fa98466fae5d363b6642341965
MinhajSadik <MinhajoSadik@gmail.com>
2022-06-28 23:42:34 +0600

    [WORKING] features for app

---
+++ backend/models/featureModel.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const featureSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: [mongoose.Schema.Types.ObjectId],
+      required: true,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    logo: {
+      type: String,
+    },
+    votes: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "User",
+    },
+    comments: {
+      type: [
+        {
+          user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+          },
+          message: {
+            type: String,
+            required: true,
+          },
+          createdAt: {
+            type: Date,
+            default: Date.now,
+          },
+        },
+      ],
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ["under-review", "planned", "in-progress", "complete"],
+    },
+    createdAt: {
+      type: Date,
+      default: new Date(),
+    },
+  },
+  { strict: false }
+);
+
+const Feature = mongoose.model("Feature", featureSchema);
+
+module.exports = Feature;
