625564a33b86a8a543919ef22468f4755d1692d9
MinhajSadik <MinhajoSadik@gmail.com>
2022-07-13 23:58:12 +0600

    [ADD] got an error in server side length is undefined

--- backend/middlewares/checkAuth.js
+++ backend/middlewares/checkAuth.js
@@ -5,6 +5,7 @@ export const checkAuthToken = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
     const isCustomAuth = token.length < 500;
+    console.log(isCustomAuth);
 
     if (!token) {
       return res.status(401).json({
@@ -17,6 +18,7 @@ export const checkAuthToken = async (req, res, next) => {
       const user = await UserModel.findById(decoded.id);
       req.user = user;
     }
+
     return next();
   } catch (error) {
     return res.status(500).json({
