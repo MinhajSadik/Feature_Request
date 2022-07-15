5e6ad26e0e494390166b8cbf76d212d1a8c3e5ae
MinhajSadik <MinhajoSadik@gmail.com>
2022-07-15 15:59:44 +0600

    [ADD] feature schema validation

--- backend/middlewares/validators/featureSchemaValidate.js
+++ backend/middlewares/validators/featureSchemaValidate.js
@@ -1,18 +1,18 @@
 import Joi from "joi";
 
 export const featureSchemaValidate = (req, res, next) => {
-  const schema = {
+  const schema = Joi.object().keys({
     title: Joi.string().required(),
     description: Joi.string().required(),
+    status: Joi.string().required(),
     logo: Joi.string().required(),
-    status: Joi.boolean().required(),
-  };
+  });
 
-  const { error } = Joi.validate(req.body, schema);
+  const { error } = schema.validate(req.body);
   if (error) {
     return res.status(400).json({
       status: 400,
-      error: error.details[0].message,
+      error: error.message,
     });
   }
   return next();
