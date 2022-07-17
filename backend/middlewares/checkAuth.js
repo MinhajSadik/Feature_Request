d0f3fdf2e86dac3db845c2874731e054bb5ca634
MinhajSadik <MinhajoSadik@gmail.com>
2022-07-17 23:20:53 +0600

    [FRONTEND/ADD] comment and upvote feature implementing

--- backend/middlewares/checkAuth.js
+++ backend/middlewares/checkAuth.js
@@ -3,7 +3,7 @@ import UserModel from "../models/userModel.js";
 
 export const checkAuthToken = async (req, res, next) => {
   try {
-    const token = req.headers.authorization;
+    const token = req.headers.authorization.split(" ")[1];
     const isCustomAuth = token.length < 500;
 
     if (token && isCustomAuth) {
