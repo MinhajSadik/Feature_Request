fc403e8278f1fc99b9887911916f74ba8d916cec
MinhajSadik <MinhajoSadik@gmail.com>
2022-07-31 23:19:51 +0600

    [WORKED] comment and vote feature update in feature_request_app

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 app.use(cookieParser());
 
 connectDB();
